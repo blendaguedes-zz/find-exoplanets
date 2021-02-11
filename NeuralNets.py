4d39b227926ce8fedd879209325ec364b8d4b485
blendaguedes <blendaguedes@hotmail.com>
2021-02-10 23:06:56 -0300

    coddigindo nomeclatura

--- NeuralNets.py
+++ NeuralNets.py
@@ -57,8 +57,10 @@ class NN3HiddenLayer(nn.Module):
         self.hl1 = act_function
         self.linear_hl2 = nn.Linear(20, 10)
         self.hl2 = act_function
-        self.output1 = nn.Linear(10, self.output_size)
-        self.output2 = nn.Sigmoid()
+        self.linear_hl3 = nn.Linear(10, 5)
+        self.hl3 = act_function
+        self.linear_hl4 = nn.Linear(5, self.output_size)
+        self.output = nn.Sigmoid()
 
     def forward(self, x):
 
@@ -66,5 +68,7 @@ class NN3HiddenLayer(nn.Module):
         x = self.hl1(x)
         x = self.linear_hl2(x)
         x = self.hl2(x)
-        x = self.output1(x)
-        return self.output2(x)
+        x = self.linear_hl3(x)
+        x = self.hl3(x)
+        x = self.linear_hl4(x)
+        return self.output(x)
